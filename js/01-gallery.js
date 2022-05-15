4ad3800de6cdf10c532852e63bc96118de5031fc
MaksymKundeev <mkundeev@gmail.com>
2022-05-15 23:32:58 +0300

    3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,92 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+let modal = basicLightbox.create(``);
+
+function creatImg({ preview, description, original }) {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+}
+
+function creatGallery(images) {
+    const gallery = images.map(creatImg);
+    return gallery.join('');
+}
+
+function addGallery(gallery) {
+   galleryEl.innerHTML = creatGallery(gallery); 
+}
+
+addGallery(galleryItems);
+
+galleryEl.addEventListener('click', openModal);
+
+function creatModal() {
+    return modal =basicLightbox.create(`
+    <img src="${event.target.src = event.target.dataset.source}"/>
+    `);
+}  
+
+function openModal(event) {
+    event.preventDefault();
+    window.addEventListener('keydown', modalCloseOnEsc);
+    
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    creatModal().show();   
+}
+  
+function modalCloseOnEsc(event) {
+    console.log(event.code);
+    
+    if (event.code === "Escape") modal.close(()=>window.removeEventListener('keydown', modalCloseOnEsc)); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function closeModalByEsc() {
+//     if(event.code === "Escape"){
+//    creatModal().close()
+//     };
+// }
+
+
+// function closeModal(){
+//      window.removeEventListener('keydown', closeModalByEsc);
+//     console.log('lightbox not visible anymore')
+// }
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+
+function creatImg({ preview, description, original }) {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}
+
+function creatGallery(images) {
+    const gallery = images.map(creatImg);
+    return gallery.join('');
+}
+
+function addGallery(gallery) {
+   galleryEl.innerHTML = creatGallery(gallery); 
+}
+
+addGallery(galleryItems);
+
+new SimpleLightbox('.gallery__item', { captionsData: "alt", captionDelay: "250"});
+
+
