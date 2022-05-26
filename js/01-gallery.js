4072f6c1be2c0824c0338f8e8c34626af9dbf198
MaksymKundeev <mkundeev@gmail.com>
2022-05-27 00:22:52 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
 const galleryEl = document.querySelector('.gallery');
 
 let modal = basicLightbox.create(``);
